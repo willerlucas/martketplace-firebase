407569c27dc715d6a790d3c5fd0d7d1a56662fde
Willer Lucas <willerlbc@gmail.com>
2020-07-08 17:25:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 var path = require('path');
 app.get('/client', function(req, res) {
